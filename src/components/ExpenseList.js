1eccf961dd01a3c35475e96d8647673698291875
Stanleys world <Stanleyugoo5@gmail.com>
2024-12-04 03:27:27 +0100

    first push on expense-tracker front-end

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ExpenseList = () => {
+    const expenses = [
+        { id: 12345, name: "Transportation", cost: $600},
+        { id: 12345, name: "Food", cost: $1000},
+        { id: 12345, name: "Utilities", cost: $300},
+        { id: 12345, name: "Debt", cost: $250},
+        { id: 12345, name: "Others", cost: $780},
+    ];
+
+    
+}
+
+export default ExpenseList;
---
+++ src/components/ExpenseTotal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ExpenseTotal = () => {
+    return (
+        <div className='alert alert-primary'>
+            <span>Spent so far: $960</span>
+
+        </div>
+    )
+}
+
+export default ExpenseTotal;
